a15553abe00d3e3551bbea6e12cc1e3f03c94b87
Uncle Bob <starnew@kakao.com>
2026-04-02 10:11:19 +0900

    tsconfig 수정

--- src/vue.shims.d.ts
+++ src/vue.shims.d.ts
@@ -1,5 +1,8 @@
+// vue.shims.d.ts
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
+  // 첫 번째와 두 번째 인자(Props와 RawBindings)에 {} 대신 object 또는 any를 사용합니다.
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const component: DefineComponent<object, object, any>
   export default component
 }
